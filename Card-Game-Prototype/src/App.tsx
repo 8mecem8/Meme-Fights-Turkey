8af8af2d90c2f777b7b49ccc0b46f04f8d5b9697
Cem.Onal <cem.onal@fdmgroup.com>
2024-11-16 03:57:26 -0400

    project set-up is done | up and running

---
+++ Card-Game-Prototype/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() 
+{
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
